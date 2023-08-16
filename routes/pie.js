4192a52894e654216654cb9cc7791756512f8595
FaresOps <116382122+FaresOps@users.noreply.github.com>
2023-08-15 12:34:30 -1200

    Update pie.js

--- routes/pie.js
+++ routes/pie.js
@@ -4,17 +4,28 @@ const router = express.Router();
 
 //get variables for graphe pie
 router.get('/', async (req, res) => {
+    try {
+        const totalexportcount = await Company.countDocuments({ exportation: true });
+        const nontotalexportcount = await Company.countDocuments({ exportation: false });
+        const multiprodcount = await Company.countDocuments({ multiproduction: true });
+        const uniqueprodcount = await Company.countDocuments({ multiproduction: false });
 
-    const totalexportcount = await Company.countDocuments({ exportation: true });
-    const nontotalexportcount = await Company.countDocuments({ exportation: false });
-    const multiprodcount = await Company.countDocuments({ multiproduction: true });
-    const uniqueprodcount = await Company.countDocuments({ multiproduction: false });
-    if (!totalexportcount && !nontotalexportcount && !multiprodcount && !uniqueprodcount) {
-        res.status(404).send({ message: 'No totalexportcount found' });
+        if (totalexportcount === 0 && nontotalexportcount === 0 && multiprodcount === 0 && uniqueprodcount === 0) {
+            return res.status(404).json({ message: 'No data found' });
+        }
+
+        const response = {
+            totalexportcount,
+            nontotalexportcount,
+            multiprodcount,
+            uniqueprodcount
+        };
+
+        res.json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while processing the data' });
     }
-    res.json({ totalexportcount, nontotalexportcount });
-    res.json({ multiprodcount, uniqueprodcount });
-})
-
+});
 
 module.exports = router
